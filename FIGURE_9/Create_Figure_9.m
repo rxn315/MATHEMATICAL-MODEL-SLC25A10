97cf50a92ffd818ca20e7df48fbd523026e40e54
rxn315 <163161944+rxn315@users.noreply.github.com>
2025-09-27 07:56:19 +0100

    Delete FIGURE_9 directory

--- FIGURE_9/Create_Figure_9.m
+++
@@ -1,9 +0,0 @@
-% ---------------- Call your figure function ----------------
-% If you saved it as createfigure_flux.m, change the function name below.
-run('run_FIG9.m')
-
-createfigure( ...
-    YData_efflux, XData1, ...  % patch 1
-    YData_influx, X1, ...      % patch 2 + time for lines
-    YMatrix1, X2, Y1);         % 3 fluxes + total
-
